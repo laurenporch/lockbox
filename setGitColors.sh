8f85e09f240d4d4c2e7d395b7598db11da9848cc
Lauren Porch <loporch@gmail.com>
2020-11-06 18:08:03 -0600

    terminal setup files

---
+++ setGitColors.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+MONTH="$(cut -d' ' -f2 <<< "$(date)")"
+
+if [ "$MONTH" == "Jan" ]; then
+    ADDED=195
+    CHANGED=81
+    UNTRACKED=33
+    UNMERGED=255
+elif [ "$MONTH" == "Nov" ]; then
+    ADDED=220
+    CHANGED=202
+    UNTRACKED=130
+    UNMERGED=196
+elif [ "$MONTH" == "Dec" ]; then
+    ADDED=46
+    CHANGED=196
+    UNTRACKED=220
+    UNMERGED=250
+else # Default colors
+    ADDED=231
+    CHANGED=232
+    UNTRACKED=249
+    UNMERGED=243
+fi
+
+sed -i -e "s/added =.*/added = $ADDED/g" ~/.gitconfig
+sed -i -e "s/changed =.*/changed = $CHANGED/g" ~/.gitconfig
+sed -i -e "s/untracked =.*/untracked = $UNTRACKED/g" ~/.gitconfig
+sed -i -e "s/unmerged =.*/unmerged = $UNMERGED/g" ~/.gitconfig
---
+++ setPromptColors.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+export TERM=xterm-256color
+function EXT_COLOR () { echo -ne "\e[$1;38;5;$2m"; }     # REQUIRED FOR GETTING 256 COLORS IN PS1 PROMPT!!! KEEP!!!!
+
+MONTH="$(cut -d' ' -f2 <<< "$(date)")"
+
+if [ "$MONTH" == "Jan" ]; then
+    MESSAGE_1="Happy"
+    MESSAGE_2="New Year"
+    MAIN_COLOR=`EXT_COLOR 1 195`
+    DI_COLOR="1;38;5;195"
+    ACCENT_COLOR=`EXT_COLOR 1 81`
+    EX_COLOR="1;38;5;81"
+    PASS_COLOR=`EXT_COLOR 1 33`
+    LN_COLOR="1;38;5;33"
+    ERROR_COLOR=`EXT_COLOR 1 255`
+elif [ "$MONTH" == "Nov" ]; then
+    MESSAGE_1="Turkey"
+    MESSAGE_2="Time"
+    MAIN_COLOR=`EXT_COLOR 1 220`
+    DI_COLOR="1;38;5;220"
+    ACCENT_COLOR=`EXT_COLOR 1 202`
+    EX_COLOR="1;38;5;202"
+    PASS_COLOR=`EXT_COLOR 1 130`
+    LN_COLOR="1;38;5;130"
+    ERROR_COLOR=`EXT_COLOR 1 196`
+elif [ "$MONTH" == "Dec" ]; then
+    MESSAGE_1="MERRY"
+    MESSAGE_2="CHRISTMAS!"
+    MAIN_COLOR=`EXT_COLOR 1 46`
+    DI_COLOR="1;38;5;46"
+    ACCENT_COLOR=`EXT_COLOR 1 196`
+    EX_COLOR="1;38;5;196"
+    PASS_COLOR=`EXT_COLOR 1 220`
+    LN_COLOR="1;38;5;220"
+    ERROR_COLOR=`EXT_COLOR 1 250`
+else # Default colors
+    MESSAGE_1="Clean"
+    MESSAGE_2="Slate"
+    MAIN_COLOR=`EXT_COLOR 1 231`
+    DI_COLOR="1;38;5;231"
+    ACCENT_COLOR=`EXT_COLOR 1 232`
+    EX_COLOR="1;38;5;232"
+    PASS_COLOR=`EXT_COLOR 1 249`
+    LN_COLOR="1;38;5;249"
+    ERROR_COLOR=`EXT_COLOR 1 243`
+fi
+
+DEFAULT_COLOR="\e[0m"
+
+echo -e $PASS_COLOR"~"$ERROR_COLOR"*"$MAIN_COLOR$MESSAGE_1 $ACCENT_COLOR$MESSAGE_2$ERROR_COLOR"*"$PASS_COLOR"~$DEFAULT_COLOR"
+
+LS_COLORS=$LS_COLORS:"di=$DI_COLOR::ex=$EX_COLOR::ln=$LN_COLOR:" ; export LS_COLORS
+
+parse_git_branch() {
+     git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
+}
+OK_TERM() {
+  echo -e "$PASS_COLOR\xE2\x9C\x94$DEFAULT_COLOR"
+}
+ERROR_TERM() {
+  echo -e "$ERROR_COLOR\xE2\x9C\x98$DEFAULT_COLOR"
+}
+DOMAIN_NAME_TERM() {
+  echo -e "$MAIN_COLOR$USER$ACCENT_COLOR@$(hostname)$DEFAULT_COLOR"
+}
+short_pwd() {
+  echo $(pwd) | sed "s/\/Users\/$USER\//~\//"
+}
+WORKING_DIR_TERM() {
+  echo -e "$MAIN_COLOR$(short_pwd)$DEFAULT_COLOR"
+}
+GIT_BRANCH_TERM() {
+  echo -e "$ACCENT_COLOR$(parse_git_branch)$DEFAULT_COLOR"
+}
+export PS1=$PASS_COLOR"┌─[\`if [ \$? = 0 ]; then echo \$(OK_TERM); else echo \$(ERROR_TERM); fi\`$PASS_COLOR]───[\$(DOMAIN_NAME_TERM)$PASS_COLOR]───[\$(WORKING_DIR_TERM)\$(GIT_BRANCH_TERM)$PASS_COLOR]\n└───▶\[$DEFAULT_COLOR\] "
